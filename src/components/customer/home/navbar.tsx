bdad7ebd67d01431e77b8a569c3b78aaceee7b87
vinit-churi <vinitchuri@hotmail.com>
2025-05-01 19:38:31 +0530

    Refactor navbar links for improved clarity and structure.
    Reorganized and simplified the JSX structure for mobile and desktop menus, ensuring consistent placement of the "LoopBill" link and removing unnecessary whitespace. This improves readability and maintainability of the codebase.

--- src/components/customer/home/navbar.tsx
+++ src/components/customer/home/navbar.tsx
@@ -10,20 +10,18 @@ const Navbar = () => {
     return (
         <header className="sticky top-0 z-50 bg-[#edebe4]">
             <div className="flex md:justify-between items-center p-4 md:py-3.5 md:px-8 relative justify-center">
-                {/* Mobile Menu Button */}
+                {/* Mobile Menu */}
                 <button className="md:hidden absolute start-4 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
                     <img className="h-6 w-6" src="/menu_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg" alt="menu"/>
                 </button>
-
-                <Link href="/" className="hidden md:flex text-2xl font-bold text-primary">
-                    LoopBill
-                </Link>
-
                 <Link href="/" className="md:hidden text-2xl font-bold text-primary">
                     LoopBill
                 </Link>
 
                 {/* Desktop Menu */}
+                <Link href="/" className="hidden md:flex text-2xl font-bold text-primary">
+                    LoopBill
+                </Link>
                 <nav className="hidden md:flex items-center space-x-8">
                     <Link href="/" className="nav-link">Home</Link>
                     <Link href={{pathname: '/', hash: 'about'}} scroll={true} className="nav-link">About</Link>
@@ -31,8 +29,6 @@ const Navbar = () => {
                     <Link href={{pathname: '/', hash: 'contact'}} scroll={true} className="nav-link">Contact</Link>
                     <Button className="btn-primary cursor-pointer">Get Started</Button>
                 </nav>
-
-
             </div>
 
             {/* Mobile Menu */}
