f661c11f3b01bd30a4b78edf72a31809dcc7c064
vinit-churi <vinitchuri@hotmail.com>
2025-05-01 22:42:24 +0530

    Refactor Navbar component for semantic improvements
    Replaced <header> with <nav> for better semantic accuracy. Adjusted structural elements and removed redundant <nav> tags. Enhanced code readability and alignment with accessibility standards.

--- src/components/customer/home/navbar.tsx
+++ src/components/customer/home/navbar.tsx
@@ -3,28 +3,23 @@ import {Button} from "@/components/ui/button";
 
 export default function Navbar() {
     return (
-        <header className="top-0 z-50 p-4 flex justify-center items-center relative md:justify-between md:py-3.5 md:px-8
+        <nav className="relative top-0 z-50 p-4 flex justify-center items-center md:justify-between md:py-3.5 md:px-8
         bg-[#edebe4]">
 
             {/* Mobile Menu */}
-            <button className="md:hidden absolute start-4 cursor-pointer">
+            <button type="button" className="md:hidden absolute start-4 cursor-pointer">
                 <img className="h-6 w-6" src="/menu_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg" alt="menu"/>
             </button>
-            <Link href="/" className="md:hidden text-2xl font-bold text-primary">
-                LoopBill
-            </Link>
+            <Link href="/" className="md:hidden text-2xl font-bold text-primary">LoopBill</Link>
 
             {/* Desktop Menu */}
-            <Link href="/" className="hidden md:flex text-2xl font-bold text-primary">
-                LoopBill
-            </Link>
-            <nav className="hidden md:flex items-center space-x-8">
-                <Link href="/" className="nav-link">Home</Link>
-                <Link href={{pathname: '/', hash: 'about'}} scroll={true} className="nav-link">About</Link>
-                <Link href={{pathname: '/', hash: 'pricing'}} scroll={true} className="nav-link">Pricing</Link>
-                <Link href={{pathname: '/', hash: 'contact'}} scroll={true} className="nav-link">Contact</Link>
+            <Link href="/" className="hidden md:flex text-2xl font-bold text-primary">LoopBill</Link>
+            <div className="hidden md:flex items-center space-x-8">
+                <Link href={{pathname: '/', hash: 'about'}} scroll={true}>About</Link>
+                <Link href={{pathname: '/', hash: 'pricing'}} scroll={true}>Pricing</Link>
+                <Link href={{pathname: '/', hash: 'contact'}} scroll={true}>Contact</Link>
                 <Button className="btn-primary cursor-pointer">Get Started</Button>
-            </nav>
-        </header>
+            </div>
+        </nav>
     );
 }
