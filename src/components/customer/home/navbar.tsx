65a371515f73eecd4bd258a60859de5fee196a89
vinit-churi <vinitchuri@hotmail.com>
2025-05-02 00:14:22 +0530

    Refactor Navbar and add mobile menu with Sheet component
    Split Navbar into separate Desktop and Mobile views for improved clarity and structure. Integrated a sliding sheet menu for the mobile view, enhancing usability and aligning with modern UI practices.

--- src/components/customer/home/navbar.tsx
+++ src/components/customer/home/navbar.tsx
@@ -3,23 +3,23 @@ import {Button} from "@/components/ui/button";
 
 export default function Navbar() {
     return (
-        <nav className="relative top-0 z-50 p-4 flex justify-center items-center md:justify-between md:py-3.5 md:px-8
-        bg-[#edebe4]">
+        <>
+            {/* Desktop View */}
+            <nav
+                className="hidden md:flex relative top-0 z-50 items-center md:justify-between md:py-3.5 md:px-8 bg-[#edebe4]">
+                <Link href="/" className=" text-2xl font-bold text-primary">LoopBill</Link>
+                <div className="items-center space-x-8">
+                    <Link href={{pathname: '/', hash: 'about'}} scroll={true}>About</Link>
+                    <Link href={{pathname: '/', hash: 'pricing'}} scroll={true}>Pricing</Link>
+                    <Link href={{pathname: '/', hash: 'contact'}} scroll={true}>Contact</Link>
+                    <Button className="btn-primary cursor-pointer">Get Started</Button>
+                </div>
+            </nav>
 
-            {/* Mobile Menu */}
-            <button type="button" className="md:hidden absolute start-4 cursor-pointer">
-                <img className="h-6 w-6" src="/menu_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg" alt="menu"/>
-            </button>
-            <Link href="/" className="md:hidden text-2xl font-bold text-primary">LoopBill</Link>
-
-            {/* Desktop Menu */}
-            <Link href="/" className="hidden md:flex text-2xl font-bold text-primary">LoopBill</Link>
-            <div className="hidden md:flex items-center space-x-8">
-                <Link href={{pathname: '/', hash: 'about'}} scroll={true}>About</Link>
-                <Link href={{pathname: '/', hash: 'pricing'}} scroll={true}>Pricing</Link>
-                <Link href={{pathname: '/', hash: 'contact'}} scroll={true}>Contact</Link>
-                <Button className="btn-primary cursor-pointer">Get Started</Button>
-            </div>
-        </nav>
+            {/* Mobile View */}
+            <nav className="md:hidden relative top-0 z-50 p-4 flex justify-center items-center bg-[#edebe4]">
+                <Link href="/" className="text-2xl font-bold text-primary">LoopBill</Link>
+            </nav>
+        </>
     );
 }
