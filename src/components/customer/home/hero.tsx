03fb8d8db98af2e1974c0c4e9b19164f832bca2a
vinit-churi <vinitchuri@hotmail.com>
2025-05-06 20:08:33 +0530

    Add hero image and adjust layout for improved design
    Introduced a new hero section image using Next.js `Image` component to enhance visual appeal. Updated layout styles for better alignment and responsiveness. Added `sprayman.svg` to the public directory for the image asset.

--- src/components/customer/home/hero.tsx
+++ src/components/customer/home/hero.tsx
@@ -1,9 +1,11 @@
 import {Button} from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Hero() {
     return (
         <header className="relative min-h-screen flex items-center justify-center bg-gradient-to-b from-[#edebe4] to-[#f7f6f0]">
-            <section className="mx-auto max-w-3xl px-4 text-center">
+            <section className="flex flex-col items-center mx-auto max-w-3xl px-4 text-center">
+                <Image alt="pest image" src="sprayman.svg" width={300} height={300} />
                 <h1 className="heading-gradient mb-6 text-4xl font-bold text-primary md:text-6xl">
                     Professional Pest Control Solutions
                 </h1>
@@ -11,7 +13,7 @@ export default function Hero() {
                     Protect your home and business from unwanted pests with our expert
                     services. Professional, reliable, and guaranteed results.
                 </p>
-                <div className="flex flex-col justify-center gap-4 sm:flex-row">
+                <div className="w-full sm:w-auto flex flex-col justify-center gap-4 sm:flex-row">
                     <Button className="btn-primary">Get Free Quote</Button>
                     <Button variant="outline">Learn More</Button>
                 </div>
