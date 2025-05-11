67b80b6b3d68cd59e2059f7c74f3c4bac88c8eb9
Vidyesh Churi <vidyesh95@gmail.com>
2025-05-11 17:27:33 +0530

    Update CSS calculation for hero section min-height
    Changed the `calc` function unit from `64px` to `4rem` for consistency with other styling units. This ensures better scalability and alignment with the design system.

--- src/components/customer/home/hero.tsx
+++ src/components/customer/home/hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Hero() {
     return (
-        <header className="min-h-[calc(100vh-64px)] flex flex-col justify-center items-center bg-gradient-to-b from-[#edebe4] to-[#f7f6f0]">
+        <header className="min-h-[calc(100vh-4rem)] flex flex-col justify-center items-center bg-gradient-to-b from-[#edebe4] to-[#f7f6f0]">
             {/*<section className="flex flex-col items-center mx-auto max-w-3xl px-4 text-center"></section>*/}
             <Image alt="pest image" src="sprayman.svg" width={300} height={300} />
             <h1 className="max-w-3xl px-4 md:text-6xl text-3xl text-center font-bold text-primary mb-4">
