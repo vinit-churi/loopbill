66af1cdbd782f06272f0813542d250bece90ca22
Vidyesh Churi <vidyesh95@gmail.com>
2025-05-11 16:28:41 +0530

    Update Hero component layout and spacing
    Replaced the relative class with py-4 for consistent spacing and adjusted the flexbox layout to flex-col for better alignment and structure. This improves the overall visual appearance and readability of the Hero section.

--- src/components/customer/home/hero.tsx
+++ src/components/customer/home/hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Hero() {
     return (
-        <header className="relative min-h-screen flex items-center justify-center bg-gradient-to-b from-[#edebe4] to-[#f7f6f0]">
+        <header className="min-h-screen py-4 flex flex-col justify-center items-center bg-gradient-to-b from-[#edebe4] to-[#f7f6f0]">
             <section className="flex flex-col items-center mx-auto max-w-3xl px-4 text-center">
                 <Image alt="pest image" src="sprayman.svg" width={300} height={300} />
                 <h1 className="heading-gradient mb-6 text-4xl font-bold text-primary md:text-6xl">
