34ab66529f0e7da910a6c67cc962c9c26596d0f0
Vidyesh Churi <vidyesh95@gmail.com>
2025-05-11 22:25:10 +0530

    Refactor admin dashboard layout and add stats card
    Refactored the dashboard structure for better alignment and spacing. Introduced a new stats card component to display key metrics, starting with total packages and their monthly growth. Updated imports to include necessary components and icons.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,16 +1,36 @@
 import {Button} from "@/components/ui/button";
-import {Calendar} from "lucide-react";
+import {Activity, Calendar} from "lucide-react";
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 
 export default function Admin() {
     return (
-        <main>
-            <div>
-                <h1 className="text-3xl font-bold">Dashboard</h1>
-                <p className="text-muted-foreground">Overview of UrbanPestMaster operations</p>
+        <main className="p-4">
+            <div className="flex justify-between">
+                <div>
+                    <h1 className="text-3xl font-bold">Dashboard</h1>
+                    <p className="text-muted-foreground">Overview of UrbanPestMaster operations</p>
+                </div>
+                <div className="flex gap-2 items-center">
+                    <Button variant="outline">Export</Button>
+                    <Button><Calendar className="h-4 w-4"/> Schedule Service</Button>
+                </div>
             </div>
-            <div className="flex gap-2 items-center">
-                <Button variant="outline">Export</Button>
-                <Button><Calendar className="h-4 w-4"/> Schedule Service</Button>
+
+            <div>
+                <Card>
+                    <CardHeader className="flex items-center justify-between">
+                        <CardTitle className="text-sm">Total Packages</CardTitle>
+                        <div className="h-8 w-8 flex justify-center items-center bg-[#b4cffb] rounded-full">
+                            <Activity color="#4387f4" size={18}/>
+                        </div>
+                    </CardHeader>
+                    <CardContent>
+                        <h1>1,248</h1>
+                    </CardContent>
+                    <CardFooter>
+                        <CardDescription>+20% from last month</CardDescription>
+                    </CardFooter>
+                </Card>
             </div>
         </main>
     );
