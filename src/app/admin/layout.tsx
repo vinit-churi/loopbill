16fe557b0f86551e4d8a29f289d9757343e19576
vinit-churi <vinitchuri@hotmail.com>
2025-05-14 14:59:53 +0530

    Make admin sidebar state persistent using cookies
    Refactored the admin layout to initialize the sidebar state based on a cookie value. This ensures the sidebar remains in its last open or closed state across sessions, improving user experience.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import {SidebarProvider} from "@/components/ui/sidebar";
 import {AppSidebar} from "@/components/admin/app-sidebar";
 import TopNav from "@/components/admin/top-navbar";
+import {cookies} from "next/headers";
 
 interface Props {
     children: React.ReactNode;
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = async ({children}: Props) => {
+    const cookieStore = await cookies();
+    const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+
     return (
-        <SidebarProvider>
+        <SidebarProvider defaultOpen={defaultOpen}>
             <div className="min-h-screen flex w-full">
-                <AppSidebar />
+                <AppSidebar/>
                 <section className="flex-1 flex flex-col">
-                    <TopNav />
+                    <TopNav/>
                     <main className="flex-1 p-4 md:p-6 overflow-auto">
                         {children}
                     </main>
