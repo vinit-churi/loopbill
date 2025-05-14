0a0050ca9cae9c621f9abe602baf44811b9f67c0
vinit-churi <vinitchuri@hotmail.com>
2025-05-14 13:12:29 +0530

    Revamp admin sidebar with dynamic menu items
    Replaced static sidebar groups with a dynamic menu driven by an items array. Integrated Lucide icons and links to provide better navigation and improve scalability for future updates. Additionally, updated the user management route for consistency.

--- src/components/admin/app-sidebar.tsx
+++ src/components/admin/app-sidebar.tsx
@@ -1,18 +1,87 @@
+import {Bell, Home, NotepadText, ReceiptText, ShieldCheck, TriangleAlert, UserRoundCog} from "lucide-react";
+
 import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
-    SidebarGroup,
-    SidebarHeader,
+    SidebarGroup, SidebarGroupContent, SidebarGroupLabel,
+    SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem,
 } from "@/components/ui/sidebar"
+
+// Menu items
+const items = [
+    {
+        id: '1',
+        label: 'Dashboard',
+        icon: Home,
+        href: '/admin',
+    },
+    {
+        id: '2',
+        label: 'Services',
+        icon: ShieldCheck,
+        href: '/admin/services',
+    },
+    {
+        id: '3',
+        label: 'Reports',
+        icon: NotepadText,
+        href: '/admin/Reports',
+    },
+    {
+        id: '4',
+        label: 'Complaints',
+        icon: TriangleAlert,
+        href: '/admin/complaints',
+    },
+    {
+        id: '5',
+        label: 'Contracts',
+        icon: ReceiptText,
+        href: '/admin/contracts',
+    },
+    {
+        id: '6',
+        label: 'User management',
+        icon: UserRoundCog,
+        href: '/admin/user-management',
+    },
+    {
+        id: '7',
+        label: 'Notifications',
+        icon: Bell,
+        href: '/admin/notifications',
+    },
+    {
+        id: '8',
+        label: 'Settings',
+        icon: ShieldCheck,
+        href: '/admin/settings',
+    }
+]
 
 export function AppSidebar() {
     return (
         <Sidebar>
             <SidebarHeader />
             <SidebarContent>
-                <SidebarGroup />
-                <SidebarGroup />
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.label}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.href}>
+                                            <item.icon />
+                                            <span>{item.label}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
             </SidebarContent>
             <SidebarFooter />
         </Sidebar>
