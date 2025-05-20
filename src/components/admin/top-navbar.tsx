c413f958dc896415c510e81eb550c724f8877e31
vinit-churi <vinitchuri@hotmail.com>
2025-05-20 18:52:09 +0530

    Add dropdown menus and animations to TopNavbar
    Introduced dropdown menus for notifications, calendar, and profile in the TopNavbar for improved functionality and interaction. Updated global CSS with a marquee animation utility and added a profile image asset to enhance the UI.

--- src/components/admin/top-navbar.tsx
+++ src/components/admin/top-navbar.tsx
@@ -2,6 +2,13 @@ import {Input} from "@/components/ui/input";
 import {SidebarTrigger} from "@/components/ui/sidebar";
 import {Bell, Calendar, Settings} from "lucide-react";
 import Image from "next/image";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 
 export default function TopNavbar() {
     return (
@@ -11,18 +18,56 @@ export default function TopNavbar() {
                 placeholder="Search..."
                 className="flex flex-1 items-center bg-[#f7f6f0] border-none"
             />
-            <div className="w-10 h-10 rounded-full border border-gray-200 flex items-center justify-center">
-                <Bell size={18}/>
-            </div>
-            <div className="w-10 h-10 rounded-full border border-gray-200 flex items-center justify-center">
-                <Calendar size={18}/>
-            </div>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <div className="w-10 h-10 rounded-full border border-gray-200
+                    flex items-center justify-center cursor-pointer">
+                        <Bell size={18}/>
+                    </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>Notifications</DropdownMenuLabel>
+                    <DropdownMenuSeparator/>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <div className="w-10 h-10 rounded-full border border-gray-200
+                    flex items-center justify-center cursor-pointer">
+                        <Calendar size={18}/>
+                    </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>Upcoming Services</DropdownMenuLabel>
+                    <DropdownMenuSeparator/>
+                </DropdownMenuContent>
+            </DropdownMenu>
             <div className="w-10 h-10 rounded-full border border-gray-200 flex items-center justify-center">
                 <Settings size={18}/>
             </div>
-            <div className="w-10 h-10 rounded-full border border-gray-200 flex items-center justify-center overflow-hidden">
-                <Image src="/file.svg" width={38} height={38} alt="Profile image"/>
-            </div>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <div className="w-10 h-10 rounded-full border border-gray-200
+            flex items-center justify-center overflow-hidden cursor-pointer">
+                        <Image src="/profile_image.png" width={38} height={38} alt="Profile image"/>
+                    </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-80">
+                    <DropdownMenuLabel>
+                        <div className="flex items-center gap-2">
+                            <div className="w-16 h-16 rounded-full border border-gray-200 flex items-center
+                            justify-center overflow-hidden cursor-pointer">
+                                <Image src="/profile_image.png" width={64} height={64} alt="Profile image"/>
+                            </div>
+                            <div>
+                                <h4 className="max-w-50 text-lg text-primary font-bold truncate">Jean McMaster</h4>
+                                <p className="max-w-50 text-sm text-muted-foreground truncate">admin.pestmaster@gmail.com</p>
+                            </div>
+                        </div>
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator/>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </header>
     );
 };
