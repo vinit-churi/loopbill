555dba9ca6ee1bf097b56d1aff6f44a3d0e83aa6
vinit-churi <vinitchuri@hotmail.com>
2025-06-11 17:20:53 +0530

    Remove `SettingsServiceConfigurationTable` component and related data from the settings page for cleanup and unused code elimination.

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -1,58 +1,6 @@
-import SettingsServiceConfigurationTable from "@/components/admin/settings/settings-service-configuration-table";
 import CompanyInformation from "@/components/admin/settings/company-information";
 import ServiceConfiguration from "@/components/admin/settings/service-configuration";
 import NotificationsSettings from "@/components/admin/settings/notifications-settings";
-
-const allServiceConfigurations = [
-    {
-        serviceId: 1,
-        serviceType: 'Cockroach Treatment',
-        serviceDuration: '30 minutes',
-        servicePrice: '₹50',
-        serviceStatus: 'Active',
-        serviceActions: 'Edit | Delete',
-    },
-    {
-        serviceId: 2,
-        serviceType: 'Termite Control',
-        serviceDuration: '45 minutes',
-        servicePrice: '₹75',
-        serviceStatus: 'Active',
-        serviceActions: 'Edit | Delete',
-    },
-    {
-        serviceId: 3,
-        serviceType: 'Bed Bug Treatment',
-        serviceDuration: '60 minutes',
-        servicePrice: '₹100',
-        serviceStatus: 'Inactive',
-        serviceActions: 'Edit | Delete',
-    },
-    {
-        serviceId: 4,
-        serviceType: 'Pest Inspection',
-        serviceDuration: '20 minutes',
-        servicePrice: '₹30',
-        serviceStatus: 'Seasonal',
-        serviceActions: 'Edit | Delete',
-    },
-    {
-        serviceId: 5,
-        serviceType: 'Mosquito Control',
-        serviceDuration: '40 minutes',
-        servicePrice: '₹60',
-        serviceStatus: 'Active',
-        serviceActions: 'Edit | Delete',
-    },
-    {
-        serviceId: 6,
-        serviceType: 'Rodent Control',
-        serviceDuration: '50 minutes',
-        servicePrice: '₹80',
-        serviceStatus: 'Active',
-        serviceActions: 'Edit | Delete',
-    }
-];
 
 export default function Settings() {
     return (
@@ -64,12 +12,9 @@ export default function Settings() {
                 </div>
             </div>
             <hr/>
-
             <CompanyInformation/>
             <ServiceConfiguration/>
             <NotificationsSettings/>
-
-            <SettingsServiceConfigurationTable serviceConfigurations={allServiceConfigurations}/>
         </main>
     );
 }
