d42160c49c15d222db843fb8e5f7a1b4e713eb61
vinit-churi <vinitchuri@hotmail.com>
2025-06-17 20:46:03 +0530

    Refactor CompanyInformation: migrate to react-hook-form, integrate controlled inputs, and improve code structure for enhanced form handling

--- src/components/admin/settings/company-information.tsx
+++ src/components/admin/settings/company-information.tsx
@@ -123,98 +123,33 @@ export default function CompanyInformation() {
     }
 
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle className={"flex gap-1"}>
-                    <Building2 size={18} color={"oklch(62.3% 0.214 259.815)"}/>Company & Branch Information
-                </CardTitle>
-                <CardDescription>Configure company and branch details in one place</CardDescription>
-            </CardHeader>
-            <CardContent className={"space-y-4"}>
-                <h4 className={"font-semibold text-blue-500 mb-2"}>Main Company</h4>
-                <hr/>
-                <div className={"space-y-2"}>
-                    <Label htmlFor={"company-name"}>Company name</Label>
-                    <Input id={"company-name"} type={"text"} placeholder={"Registered Company name"}/>
-                </div>
-                <div className={"space-y-2"}>
-                    <Label htmlFor={"company-address"}>Company address</Label>
-                    <Textarea id={"company-address"} placeholder={"Type your company address here"}/>
-                </div>
-                <div className={"flex flex-row items-center justify-between"}>
-                    <h4 className={"text-sm font-semibold"}>Phone</h4>
-                    <Button variant={"outline"} className={"cursor-pointer"}><Plus/>Add phone</Button>
-                </div>
-                <div className={"flex flex-row gap-2"}>
-                    <Input id={"phone-type"} type={"text"} placeholder={"Phone type"} className={"w-30"}/>
-                    <Input id={"phone-number"} type={"tel"} placeholder={"Phone number"}/>
-                    <Button variant={"outline"} className={"cursor-pointer"}>
-                        <Trash2 color={"oklch(63.7% 0.237 25.331)"}/>
-                    </Button>
-                </div>
-                <div className={"flex flex-row items-center justify-between"}>
-                    <h4 className={"text-sm font-semibold"}>Email</h4>
-                    <Button variant={"outline"} className={"cursor-pointer"}><Plus/>Add email</Button>
-                </div>
-                <div className={"flex flex-row gap-2"}>
-                    <Input id={"email-type"} type={"text"} placeholder={"Email type"} className={"w-30"}/>
-                    <Input id={"email-address"} type={"tel"} placeholder={"Email address"}/>
-                    <Button variant={"outline"} className={"cursor-pointer"}>
-                        <Trash2 color={"oklch(63.7% 0.237 25.331)"}/>
-                    </Button>
-                </div>
-                <h4 className={"text-sm font-semibold"}>Main office hours</h4>
-                <div className={"grid grid-cols-1 md:grid-cols-2 gap-4"}>
-                    <div className={"flex flex-row gap-2"}>
-                        <Label htmlFor={"monday"} className={"w-30"}>Monday:</Label>
-                        <Input id={"monday"} type={"text"} placeholder={"Opening time - Closing time"}/>
-                    </div>
-                    <div className={"flex flex-row gap-2"}>
-                        <Label htmlFor={"tuesday"} className={"w-30"}>Tuesday:</Label>
-                        <Input id={"tuesday"} type={"text"} placeholder={"Opening time - Closing time"}/>
-                    </div>
-                    <div className={"flex flex-row gap-2"}>
-                        <Label htmlFor={"wednesday"} className={"w-30"}>Wednesday:</Label>
-                        <Input id={"wednesday"} type={"text"} placeholder={"Opening time - Closing time"}/>
-                    </div>
-                    <div className={"flex flex-row gap-2"}>
-                        <Label htmlFor={"thursday"} className={"w-30"}>Thursday:</Label>
-                        <Input id={"thursday"} type={"text"} placeholder={"Opening time - Closing time"}/>
-                    </div>
-                    <div className={"flex flex-row gap-2"}>
-                        <Label htmlFor={"friday"} className={"w-30"}>Friday:</Label>
-                        <Input id={"friday"} type={"text"} placeholder={"Opening time - Closing time"}/>
-                    </div>
-                    <div className={"flex flex-row gap-2"}>
-                        <Label htmlFor={"saturday"} className={"w-30"}>Saturday:</Label>
-                        <Input id={"saturday"} type={"text"} placeholder={"Opening time - Closing time"}/>
-                    </div>
-                    <div className={"flex flex-row gap-2"}>
-                        <Label htmlFor={"sunday"} className={"w-30"}>Sunday:</Label>
-                        <Input id={"sunday"} type={"text"} placeholder={"Opening time - Closing time"}/>
-                    </div>
-                </div>
-
-                <div className={"flex flex-row items-center justify-between"}>
-                    <h4 className={"font-semibold text-green-500 mb-2"}>Branches</h4>
-                    <Button variant={"outline"} className={"cursor-pointer"}><Plus/>Add branch</Button>
-                </div>
-                <hr/>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
                 <Card>
                     <CardHeader>
-                        <CardTitle>Branch 1</CardTitle>
-                        <CardAction><Button variant={"outline"}>
-                            <Trash2 color={"oklch(63.7% 0.237 25.331)"}/></Button>
-                        </CardAction>
+                        <CardTitle className={"flex gap-1"}>
+                            <Building2 size={18} color={"oklch(62.3% 0.214 259.815)"}/>Company & Branch Information
+                        </CardTitle>
+                        <CardDescription>Configure company and branch details in one place</CardDescription>
                     </CardHeader>
                     <CardContent className={"space-y-4"}>
+                        <h4 className={"font-semibold text-blue-500 mb-2"}>Main Company</h4>
+                        <hr/>
+                        <FormField
+                            control={form.control}
+                            name={"companyName"}
+                            render={({field}) => (
+                                <FormItem className={"space-y-2"}>
+                                    <FormLabel htmlFor={"company-name"}>Company name</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} id={"company-name"} type={"text"} placeholder={"Registered Company name"}/>
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
                         <div className={"space-y-2"}>
-                            <Label htmlFor={"branch-name"}>Branch name</Label>
-                            <Input id={"branch-name"} type={"text"} placeholder={"Branch name"}/>
-                        </div>
-                        <div className={"space-y-2"}>
-                            <Label htmlFor={"branch-address"}>Branch address</Label>
-                            <Textarea id={"branch-address"} placeholder={"Type your branch address here"}/>
+                            <Label htmlFor={"company-address"}>Company address</Label>
+                            <Textarea id={"company-address"} placeholder={"Type your company address here"}/>
                         </div>
                         <div className={"flex flex-row items-center justify-between"}>
                             <h4 className={"text-sm font-semibold"}>Phone</h4>
@@ -238,7 +173,7 @@ export default function CompanyInformation() {
                                 <Trash2 color={"oklch(63.7% 0.237 25.331)"}/>
                             </Button>
                         </div>
-                        <h4 className={"text-sm font-semibold"}>Branch office hours</h4>
+                        <h4 className={"text-sm font-semibold"}>Main office hours</h4>
                         <div className={"grid grid-cols-1 md:grid-cols-2 gap-4"}>
                             <div className={"flex flex-row gap-2"}>
                                 <Label htmlFor={"monday"} className={"w-30"}>Monday:</Label>
@@ -269,12 +204,95 @@ export default function CompanyInformation() {
                                 <Input id={"sunday"} type={"text"} placeholder={"Opening time - Closing time"}/>
                             </div>
                         </div>
+
+                        <div className={"flex flex-row items-center justify-between"}>
+                            <h4 className={"font-semibold text-green-500 mb-2"}>Branches</h4>
+                            <Button variant={"outline"} className={"cursor-pointer"}><Plus/>Add branch</Button>
+                        </div>
+                        <hr/>
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Branch 1</CardTitle>
+                                <CardAction><Button variant={"outline"}>
+                                    <Trash2 color={"oklch(63.7% 0.237 25.331)"}/></Button>
+                                </CardAction>
+                            </CardHeader>
+                            <CardContent className={"space-y-4"}>
+                                <div className={"space-y-2"}>
+                                    <Label htmlFor={"branch-name"}>Branch name</Label>
+                                    <Input id={"branch-name"} type={"text"} placeholder={"Branch name"}/>
+                                </div>
+                                <div className={"space-y-2"}>
+                                    <Label htmlFor={"branch-address"}>Branch address</Label>
+                                    <Textarea id={"branch-address"} placeholder={"Type your branch address here"}/>
+                                </div>
+                                <div className={"flex flex-row items-center justify-between"}>
+                                    <h4 className={"text-sm font-semibold"}>Phone</h4>
+                                    <Button variant={"outline"} className={"cursor-pointer"}><Plus/>Add phone</Button>
+                                </div>
+                                <div className={"flex flex-row gap-2"}>
+                                    <Input id={"phone-type"} type={"text"} placeholder={"Phone type"}
+                                           className={"w-30"}/>
+                                    <Input id={"phone-number"} type={"tel"} placeholder={"Phone number"}/>
+                                    <Button variant={"outline"} className={"cursor-pointer"}>
+                                        <Trash2 color={"oklch(63.7% 0.237 25.331)"}/>
+                                    </Button>
+                                </div>
+                                <div className={"flex flex-row items-center justify-between"}>
+                                    <h4 className={"text-sm font-semibold"}>Email</h4>
+                                    <Button variant={"outline"} className={"cursor-pointer"}><Plus/>Add email</Button>
+                                </div>
+                                <div className={"flex flex-row gap-2"}>
+                                    <Input id={"email-type"} type={"text"} placeholder={"Email type"}
+                                           className={"w-30"}/>
+                                    <Input id={"email-address"} type={"tel"} placeholder={"Email address"}/>
+                                    <Button variant={"outline"} className={"cursor-pointer"}>
+                                        <Trash2 color={"oklch(63.7% 0.237 25.331)"}/>
+                                    </Button>
+                                </div>
+                                <h4 className={"text-sm font-semibold"}>Branch office hours</h4>
+                                <div className={"grid grid-cols-1 md:grid-cols-2 gap-4"}>
+                                    <div className={"flex flex-row gap-2"}>
+                                        <Label htmlFor={"monday"} className={"w-30"}>Monday:</Label>
+                                        <Input id={"monday"} type={"text"} placeholder={"Opening time - Closing time"}/>
+                                    </div>
+                                    <div className={"flex flex-row gap-2"}>
+                                        <Label htmlFor={"tuesday"} className={"w-30"}>Tuesday:</Label>
+                                        <Input id={"tuesday"} type={"text"}
+                                               placeholder={"Opening time - Closing time"}/>
+                                    </div>
+                                    <div className={"flex flex-row gap-2"}>
+                                        <Label htmlFor={"wednesday"} className={"w-30"}>Wednesday:</Label>
+                                        <Input id={"wednesday"} type={"text"}
+                                               placeholder={"Opening time - Closing time"}/>
+                                    </div>
+                                    <div className={"flex flex-row gap-2"}>
+                                        <Label htmlFor={"thursday"} className={"w-30"}>Thursday:</Label>
+                                        <Input id={"thursday"} type={"text"}
+                                               placeholder={"Opening time - Closing time"}/>
+                                    </div>
+                                    <div className={"flex flex-row gap-2"}>
+                                        <Label htmlFor={"friday"} className={"w-30"}>Friday:</Label>
+                                        <Input id={"friday"} type={"text"} placeholder={"Opening time - Closing time"}/>
+                                    </div>
+                                    <div className={"flex flex-row gap-2"}>
+                                        <Label htmlFor={"saturday"} className={"w-30"}>Saturday:</Label>
+                                        <Input id={"saturday"} type={"text"}
+                                               placeholder={"Opening time - Closing time"}/>
+                                    </div>
+                                    <div className={"flex flex-row gap-2"}>
+                                        <Label htmlFor={"sunday"} className={"w-30"}>Sunday:</Label>
+                                        <Input id={"sunday"} type={"text"} placeholder={"Opening time - Closing time"}/>
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
                     </CardContent>
+                    <CardFooter>
+                        <Button type={"submit"} className={"w-full cursor-pointer"}>Save changes</Button>
+                    </CardFooter>
                 </Card>
-            </CardContent>
-            <CardFooter>
-                <Button type={"submit"} className={"w-full cursor-pointer"}>Save changes</Button>
-            </CardFooter>
-        </Card>
+            </form>
+        </Form>
     );
 }
