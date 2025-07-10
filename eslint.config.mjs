b64f9d9e47f984c5dda7e27849f34d255cbc1688
vinit-churi <vinitchuri@hotmail.com>
2025-07-10 19:31:45 +0530

    Add custom ESLint rules for stricter code quality and consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,20 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/no-unescaped-entities": "warn"
+    }
+  }
 ];
 
 export default eslintConfig;
